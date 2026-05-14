c4b4088b8afdb8fa497e3aa84a8e975cd0091fa7
nort-17 <anbzimports@gmail.com>
2026-05-14 20:33:55 +0700

    Improve fat loss reset lead flow

--- fat-loss-reset/_next/static/bIPrYZifvFAUcOnu8qCJb/_buildManifest.js
+++ fat-loss-reset/_next/static/bIPrYZifvFAUcOnu8qCJb/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/fat-loss-reset//_next/:path+",
-        "destination": "/fat-loss-reset/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
