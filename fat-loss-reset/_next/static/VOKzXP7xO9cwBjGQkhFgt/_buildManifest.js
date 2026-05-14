070cceacc187539d48a2c8e1d99f3e6929149b38
nort-17 <anbzimports@gmail.com>
2026-05-15 05:53:18 +0700

    Add tonight and bad night recovery sections

--- fat-loss-reset/_next/static/VOKzXP7xO9cwBjGQkhFgt/_buildManifest.js
+++ fat-loss-reset/_next/static/VOKzXP7xO9cwBjGQkhFgt/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/fat-loss-reset//_next/:path+",
+        "destination": "/fat-loss-reset/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
