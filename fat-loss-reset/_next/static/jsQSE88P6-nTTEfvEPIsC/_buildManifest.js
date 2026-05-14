571306b70a69e9eb73e943edc76378b1b6c8fa42
nort-17 <anbzimports@gmail.com>
2026-05-14 12:54:14 +0700

    Add fat loss reset tool

---
+++ fat-loss-reset/_next/static/jsQSE88P6-nTTEfvEPIsC/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/fat-loss-reset//_next/:path+",
+        "destination": "/fat-loss-reset/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
